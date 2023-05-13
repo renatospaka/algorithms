230459b6919cfdb386db586dad811b578ff0154a
Renato Spakauskas <renatospaka@gmail.com>
2023-05-13 14:49:12 -0300

    feat: :necktie: best time to buy and sell stock
    two new faster solutions

--- 121-best-time-to-buy-and-sell-stock/main.go
+++ 121-best-time-to-buy-and-sell-stock/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 
@@ -10,19 +13,19 @@ func main() {
 	)
 
 	prices = []int{7, 1, 5, 3, 6, 4}
-	bestDay = maxProfit(prices)
+	bestDay = maxProfit3(prices)
 	fmt.Printf("The maximum profit for %v is to sell in %d\n", prices, bestDay)
 
 	prices = []int{7, 6, 4, 3, 1}
-	bestDay = maxProfit(prices)
+	bestDay = maxProfit3(prices)
 	fmt.Printf("The maximum profit for %v is to sell in %d\n", prices, bestDay)
 
 	prices = []int{7, 7}
-	bestDay = maxProfit(prices)
+	bestDay = maxProfit3(prices)
 	fmt.Printf("The maximum profit for %v is to sell in %d\n", prices, bestDay)
 
 	prices = []int{}
-	bestDay = maxProfit(prices)
+	bestDay = maxProfit3(prices)
 	fmt.Printf("The maximum profit for %v is to sell in %d\n", prices, bestDay)
 }
 
@@ -45,4 +48,42 @@ func maxProfit(prices []int) int {
 	}
 
 	return maxProfit
+}
+
+func maxProfit2(prices []int) int {
+	min := math.MaxUint32
+	maxProfit := 0
+
+	for _, price := range prices {
+		if price > min {
+			if price - min > maxProfit {
+				maxProfit = price - min
+			}
+		} else {
+			min = price
+		}
+	}
+
+	return maxProfit
+}
+
+func maxProfit3(prices []int) int {
+	if len(prices) < 1 {
+		return 0
+	}
+
+	maxProfit := 0
+	minPrice := prices[0]
+
+	for i := 1; i < len(prices); i++ {
+		if prices[1] < minPrice {
+			minPrice = prices[i]
+		}
+
+		if prices[i] - minPrice > maxProfit {
+			maxProfit = prices[i] - minPrice
+		}
+	}
+
+	return maxProfit 
 }
