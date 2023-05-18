bd3b1258dbe1f5e0bda8b1262e347e168cfa3ded
Renato Spakauskas <renatospaka@gmail.com>
2023-05-18 13:34:56 -0300

    chore: :bulb: group anagrams
    commenting the code

--- 49-group-anagrams/main.go
+++ 49-group-anagrams/main.go
@@ -12,24 +12,25 @@ func main() {
 		anagrams [][]string
 	)
 
-	strs = []string{"eat","tea","tan","ate","nat","bat"}
+	strs = []string{"tab", "eat","tea","tan","ate","nat","bat"}
 	anagrams = groupAnagrams(strs)
 	fmt.Printf("Anagrams for %v are (if any): %v\n", strs, anagrams)
 
-	strs = []string{"a"}
-	anagrams = groupAnagrams(strs)
-	fmt.Printf("Anagrams for %v are (if any): %v\n", strs, anagrams)
+	// strs = []string{"a"}
+	// anagrams = groupAnagrams(strs)
+	// fmt.Printf("Anagrams for %v are (if any): %v\n", strs, anagrams)
 
-	strs = []string{""}
-	anagrams = groupAnagrams(strs)
-	fmt.Printf("Anagrams for %v are (if any): %v\n", strs, anagrams)
+	// strs = []string{""}
+	// anagrams = groupAnagrams(strs)
+	// fmt.Printf("Anagrams for %v are (if any): %v\n", strs, anagrams)
 
-	strs = []string{"rato","tora","rota","otra","atri","gato"}
-	anagrams = groupAnagrams(strs)
-	fmt.Printf("Anagrams for %v are (if any): %v\n", strs, anagrams)
+	// strs = []string{"rato","tora","rota","otra","atri","gato"}
+	// anagrams = groupAnagrams(strs)
+	// fmt.Printf("Anagrams for %v are (if any): %v\n", strs, anagrams)
 }
 
 func groupAnagrams(strs []string) [][]string {
+	fmt.Printf("strs: %v\n", strs)
   words := make(map[string][]string)
 	for _, word := range strs {
 		key := sorting(word)
@@ -37,7 +38,7 @@ func groupAnagrams(strs []string) [][]string {
 			words[key] = []string{}
 		}
 		words[key] = append(words[key], word)
-		// fmt.Printf("word: %s | words[key]: %v | key: %s\n", word, words[key], key)
+		fmt.Printf("word: %s || key: %s || words[key]: %v || words[]: %v\n", word, key, words[key], words)
 	}
 
 	result := make([][]string, len(words))
