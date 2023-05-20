392248a507ad5c2d6bfc0ab6882fda364d7ac90b
Renato Spakauskas <renatospaka@gmail.com>
2023-05-20 11:45:56 -0300

    refactor: :bug: add two numbers
    applyied the problem in the leet code platform
    
    it works fine, but I failed to pass parameters and I need to figure out how to cast an array to a inked list

--- 2-add-two-numbers/main.go
+++ 2-add-two-numbers/main.go
@@ -2,167 +2,86 @@ package main
 
 import "fmt"
 
-type Node struct {
-	Value int
-	Next *Node
-}
-
-type ListNode struct {
-	Head *Node
-}
-
-// type ListNode struct {
+// type Node struct {
 // 	Value int
-// 	Next *ListNode
+// 	Next *Node
 // }
 
-func main() {
-	list1 := &ListNode{}
-	list1.insert(2)
-	list1.insert(4)
-	list1.insert(3)
-	list1.show()
-	// listNode1 := list1.toListNode()
-	
-	list2 := &ListNode{}
-	list2.insert(5)
-	list2.insert(6)
-	list2.insert(4)
-	list2.show()
-	// listNode2 := list2.toListNode()
+// type ListNode struct {
+// 	Head *Node
+// }
 
-	newList := addTwoNumbers(listNode1, listNode2)
-	newList.show()
+type ListNode struct {
+	Val int
+	Next *ListNode
+}
+
+func main() {
+	list1 := []int{2,4,3}
+	list2 := []int{2,4,3}
+	// need to transform list1 and list2 in ListNode
+	// output := addTwoNumbers(list1, list2)
+	output := []int{7,0,8}
+	fmt.Printf("The sum of %v and %v is %v\n", list1, list2, output)
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	var (
-		head1 = l1.Head
-		head2 = l2.Head
+	dummy := &ListNode{}
+	current := dummy
+	carry := 0
 
-		newLN1 = head1
-		newLN2 = head2
-
-		head = new(ListNode)
-		current = head
-
-		diff int
-		sum int
-	)
-	
-	for newLN1 != nil || newLN2 != nil {
-		var (
-			val1 int
-			val2 int
-		)
-		
-		if newLN1 != nil {
-			val1 = newLN1.Value
-			newLN1 = newLN1.Next
-		}
-		
-		if newLN2 != nil {
-			val2 = newLN2.Value
-			newLN2 = newLN2.Next
+	for l1 != nil || l2 != nil || carry != 0 {
+		value1 := 0
+		if l1 != nil {
+			value1 = l1.Val
 		}
 
-		sum = diff + val1 + val2
-		diff = sum / 10
-		// // if current.Head == nil {
-		// // 	node := &Node{Value: sum % 10}
-		// // 	current.Head = node
-		// // }
-		node := &Node{Value: sum % 10}
-		current.Head.Next = node
-		current = current.Head.Next
-	}
-
-	if diff > 0 {
-		current.Next = &ListNode{Value: diff}
-	}
-
-	return head.Next
-}
-
-func addTwoNumbersOrig(l1 *ListNode, l2 *ListNode) *ListNode {
-	var (
-		newLN1 = l1
-		newLN2 = l1
-
-		head = new(ListNode)
-		current = head
-
-		diff int
-		sum int
-	)
-	
-	for newLN1 != nil || newLN2 != nil {
-		var (
-			val1 int
-			val2 int
-		)
-		
-		if newLN1 != nil {
-			val1 = newLN1.Value
-			newLN1 = newLN1.Next
-		}
-		
-		if newLN2 != nil {
-			val2 = newLN2.Value
-			newLN2 = newLN2.Next
+		value2 := 0
+		if l2 != nil {
+			value2 = l2.Val
 		}
 
-		sum = diff + val1 + val2
-		diff = sum / 10
-		current.Next = &ListNode{Value: sum % 10}
+		value := value1 + value2 + carry
+		carry = value / 10
+		value = value % 10
+		current.Next = &ListNode{Val: value}
+
 		current = current.Next
-	}
-
-	if diff > 0 {
-		current.Next = &ListNode{Value: diff}
-	}
-
-	return head.Next
-}
-
-
-func (l *ListNode) insert(value int) {
-	list := &Node{Value: value, Next: nil}
-	if l.Head == nil {
-		l.Head = list
-	} else {
-		pointer := l.Head
-		for pointer.Next != nil {
-			pointer = pointer.Next
+		if l1 != nil {
+			l1 = l1.Next
+		} else {
+			l1 = nil
 		}
-		pointer.Next = list
+
+		if l2 != nil {
+			l2 = l2.Next
+		} else {
+			l2 = nil
+		}
 	}
+
+	return dummy.Next
 }
 
-func (l *ListNode) show() {
-	head := l.Head
-	for head != nil {
-		fmt.Printf("-> %v", head.Value)
-		head = head.Next
-	}
-	fmt.Println("")
-}
 
-// func (l *ListNode) toListNode() *ListNode {
-// 	listNode := &ListNode{}
-// 	pointer := l.Head
-// 	for pointer.Next != nil {
-// 		fmt.Printf("Value: %d   Next: %v\n", pointer.Value, pointer.Next)
-// 		listNode.Value = pointer.Value
-// 		listNode.Next = pointer.Next.Next
-// 		pointer = pointer.Next
+// func (l *ListNode) insert(value int) {
+// 	list := &Node{Value: value, Next: nil}
+// 	if l.Head == nil {
+// 		l.Head = list
+// 	} else {
+// 		pointer := l.Head
+// 		for pointer.Next != nil {
+// 			pointer = pointer.Next
+// 		}
+// 		pointer.Next = list
 // 	}
-// 	return listNode
 // }
 
 // func (l *ListNode) show() {
-// 	fmt.Println("=============================")
-// 	for l.Next != nil {
-// 		fmt.Printf("-> %v", l.Value)
+// 	head := l.Head
+// 	for head != nil {
+// 		fmt.Printf("-> %v", head.Value)
+// 		head = head.Next
 // 	}
+// 	fmt.Println("")
 // }
