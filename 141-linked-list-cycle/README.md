f695a19a0d4fd0e52d7cb5b6c5a975f8d5304a49
Renato Spakauskas <renatospaka@gmail.com>
2025-10-22 16:54:11 -0300

    refactor: :recycle: refactored to accept input
    add NeedCode’s explanatory video

--- 141-linked-list-cycle/README.md
+++ 141-linked-list-cycle/README.md
@@ -3,3 +3,6 @@
 
 <a href="https://leetcode.com/problems/linked-list-cycle/">problem</a>
 </br><p>This is an exemple of Floyd's Tortoise and Hare circle => O(n)</p>
+
+<a href="https://www.youtube.com/watch?v=yiC-IohPLAU">explanation</a>
+</br><p>Leetcode 141. Linked List Cycle, Two Pointers Technique, Python, Linked List</p>
--- 141-linked-list-cycle/main.go
+++ 141-linked-list-cycle/main.go
@@ -1,12 +1,21 @@
 package main
 
+import "fmt"
+
 type ListNode struct {
-	Val int
+	Val  int
 	Next *ListNode
 }
 
 func main() {
+	list1 := test1()
+	fmt.Printf("is the 1st provided list cickled? %t\n", list1)
 
+	list2 := test2()
+	fmt.Printf("is the 2nd provided list cickled? %t\n", list2)
+
+	list3 := test3()
+	fmt.Printf("is the 3rd provided list cickled? %t\n", list3)
 }
 
 func hasCycle(head *ListNode) bool {
@@ -27,4 +36,26 @@ func hasCycle(head *ListNode) bool {
 	return false
 }
 
-// Detect Cycle in a Linked List
+func test1() bool {
+	node4 := &ListNode{Val: -4, Next: nil}
+	node3 := &ListNode{Val: 0, Next: node4}
+	node2 := &ListNode{Val: 2, Next: node3}
+	head := &ListNode{Val: 3, Next: node2}
+	node4.Next = node2
+
+	return hasCycle(head)
+}
+
+func test2() bool {
+	node2 := &ListNode{Val: 1, Next: nil}
+	head := &ListNode{Val: 2, Next: node2}
+	node2.Next = head
+
+	return hasCycle(head)
+}
+
+func test3() bool {
+	head := &ListNode{Val: 1, Next: nil}
+
+	return hasCycle(head)
+}
