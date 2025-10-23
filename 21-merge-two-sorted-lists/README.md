01d47a0d3547fe10d65a451bb7974db4e3379d61
Renato Spakauskas <renatospaka@gmail.com>
2025-10-23 14:49:04 -0300

    refactor: ♻ refactored to accept input
    add NeedCode’s explanatory video

--- 21-merge-two-sorted-lists/README.md
+++ 21-merge-two-sorted-lists/README.md
@@ -1,4 +1,26 @@
-## Merge Two Sorted Lists
-(easy)
 
-<a href="https://leetcode.com/problems/merge-two-sorted-lists/">problem</a>
+## 21. Merge Two Sorted Lists
+
+### Problem
+Merge two sorted linked lists and return it as a new sorted list. ([LeetCode problem](https://leetcode.com/problems/merge-two-sorted-lists/))
+
+### Approach
+This solution iteratively merges two sorted linked lists in $O(n)$ time and $O(1)$ space.
+
+**Algorithm:**
+- Initialize a dummy node and a tail pointer.
+- Traverse both lists, always attaching the smaller node to the merged list.
+- When one list is exhausted, attach the remainder of the other list.
+- Return the merged list starting from the next of the dummy node.
+
+### Example
+List 1: `1 -> 2 -> 4`
+List 2: `1 -> 3 -> 4`
+Merged list: `1 -> 1 -> 2 -> 3 -> 4 -> 4`
+
+### Usage
+See `main.go` for code to create linked lists and merge them.
+
+### Resources
+- [LeetCode problem](https://leetcode.com/problems/merge-two-sorted-lists/)
+- [Merge Two Sorted Lists explanation (YouTube)](https://youtu.be/XIdigk956u0?si=sni8g-vx7xptCsLw)
--- 21-merge-two-sorted-lists/main.go
+++ 21-merge-two-sorted-lists/main.go
@@ -1,18 +1,73 @@
 package main
 
-type ListNode struct {
-	Val int
-	Next *ListNode
-}
+import (
+	"fmt"
+
+	"github.com/renatospaka/linkedlist"
+)
+
+var (
+	firstList  *linkedlist.ListNode
+	secondList *linkedlist.ListNode
+	merged     *linkedlist.ListNode
+	n1, n2     []int
+)
 
 func main() {
+	n1 = []int{1, 2, 4}
+	n2 = []int{1, 3, 4}
+	test(n1, n2)
 
+	n1 = nil
+	n2 = []int{1, 2}
+	test(n1, n2)
+
+	n1 = nil
+	n2 = nil
+	test(n1, n2)
 }
 
-func mergeTwoList(list1 *ListNode, list2 *ListNode) *ListNode {
-	temp := new(ListNode)
-	tail := temp
+func test(numbers1, numbers2 []int) {
+	if numbers1 == nil || len(numbers1) == 0 {
+		firstList = nil
+	} else {
+		firstList = linkedlist.NewListNode()
+		firstList.Push(numbers1)
+	}
 
+	if numbers2 == nil || len(numbers2) == 0 {
+		secondList = nil
+	} else {
+		secondList = linkedlist.NewListNode()
+		secondList.Push(numbers2)
+	}
+
+	merged = mergeTwoList(firstList, secondList)
+	fmt.Printf("Merge of %v and %v is %v\n", numbers1, numbers2, merged.ToArray())
+	fmt.Println("======================================")
+	fmt.Println()
+}
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func mergeTwoList(list1 *linkedlist.ListNode, list2 *linkedlist.ListNode) *linkedlist.ListNode {
+	if list1 == nil && list2 == nil {
+		return nil
+	}
+	if list1 == nil {
+		return list2
+	}
+	if list2 == nil {
+		return list1
+	}
+
+	temp := new(linkedlist.ListNode)
+	tail := temp
 	for list1 != nil && list2 != nil {
 		if list1.Val < list2.Val {
 			tail.Next = list1
@@ -34,38 +89,14 @@ func mergeTwoList(list1 *ListNode, list2 *ListNode) *ListNode {
 	return temp.Next
 }
 
-func mergeTwoList2(list1 *ListNode, list2 *ListNode) *ListNode {
-	if list1 == nil || list2 == nil {
-		return &ListNode{}
-	}
-
-	pointer1, pointer2 := list1, list2
-	processed := new(ListNode)
-	temp := processed
-
-	for pointer1 != nil && pointer2 != nil {
-		if pointer1.Val < pointer2.Val {
-			temp.Next = pointer1
-			temp = temp.Next
-			pointer1 = pointer1.Next
+func printLog(id int, antesDepois string, list *linkedlist.ListNode) {
+	if list != nil {
+		if list.Next != nil {
+			fmt.Printf("%s=> list%d.Val: %d | list%d.Next.Val: %d\n", antesDepois, id, list.Val, id, list.Next.Val)
 		} else {
-			temp.Next = pointer2
-			temp = temp.Next
-			pointer2 = pointer2.Next
+			fmt.Printf("%s=> list%d.Val: %d | list%d.Next is nil\n", antesDepois, id, list.Val, id)
 		}
+	} else {
+		fmt.Printf("%s=> list%d is nil\n", antesDepois, id)
 	}
-
-	for pointer1 != nil {
-		temp.Next = pointer1
-		temp = temp.Next
-		pointer1 = pointer1.Next
-	}
-
-	for pointer2 != nil {
-		temp.Next = pointer2
-		temp = temp.Next
-		pointer2 = pointer2.Next
-	}
-
-	return temp.Next
 }
