ad0e617617c90320894188e1b2097c2e9ed1d912
Renato Spakauskas <renatospaka@gmail.com>
2025-10-24 11:26:29 -0300

    refactor: ♻ refactored to accept input
    add NeedCode’s explanatory video

--- 19-remove-nth-node-from-end-of-list/README.md
+++ 19-remove-nth-node-from-end-of-list/README.md
@@ -1,4 +1,26 @@
-## Remove Nth Node From End of List
-(medium)
 
-<a href="https://leetcode.com/problems/remove-nth-node-from-end-of-list/">problem</a>
+## 19. Remove Nth Node From End of List
+
+### Problem
+Remove the nth node from the end of a singly linked list. ([LeetCode problem](https://leetcode.com/problems/remove-nth-node-from-end-of-list/))
+
+### Approach
+This solution removes the nth node from the end in-place with $O(n)$ time and $O(1)$ space.
+
+**Algorithm:**
+- Use two pointers (slow and fast) to traverse the list.
+- Move the slow pointer to the end, counting steps.
+- Move the fast pointer only after the first n steps.
+- When slow reaches the end, fast points to the node before the one to remove.
+- Adjust pointers to remove the target node.
+
+### Example
+Input: `1 -> 2 -> 3 -> 4 -> 5`, n = 2  
+Output: `1 -> 2 -> 3 -> 5`
+
+### Usage
+See `main.go` for code to create a linked list and remove the nth node from the end.
+
+### Resources
+- [LeetCode problem](https://leetcode.com/problems/remove-nth-node-from-end-of-list/)
+- [Remove Nth Node explanation (YouTube)](https://youtu.be/XVuQxVej6y8?si=Ew3NQ-zRlobgEh1t)
--- 19-remove-nth-node-from-end-of-list/main.go
+++ 19-remove-nth-node-from-end-of-list/main.go
@@ -1,29 +1,92 @@
 package main
 
-type ListNode struct {
-	Val int
-	Next *ListNode
-}
+import (
+	"fmt"
+
+	"github.com/renatospaka/linkedlist"
+)
+
+var (
+	arranged *linkedlist.ListNode
+	numbers  []int
+	n        int
+)
 
 func main() {
+	numbers = []int{1, 2, 3, 4, 5}
+	n = 2
+	test(numbers, n)
 
+	numbers = []int{1}
+	n = 1
+	test(numbers, n)
+
+	numbers = []int{1, 2}
+	n = 1
+	test(numbers, n)
 }
 
-func removeNthFromEnd(head *ListNode, n int) *ListNode {
-	slow, fast, temp := head, head, 0
+func test(numbers []int, pos int) {
+	list := linkedlist.NewListNode()
+	list.Push(numbers)
+
+	arranged = removeNthFromEnd(list, pos)
+	fmt.Printf("List with %dth item removed from %v is %v\n", pos, numbers, arranged.ToArray())
+	fmt.Println("===================================================")
+	fmt.Println()
+	fmt.Println()
+}
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func removeNthFromEnd(head *linkedlist.ListNode, n int) *linkedlist.ListNode {
+	slow, fast, temp, count := head, head, 0, 0
 	for slow != nil {
+		fmt.Printf("%s | n: %d | ", "S", n)
+		printLog(temp, slow, fast)
 		if temp > n {
+			fmt.Println("temp is greather than n")
 			fast = fast.Next
-		} 
+		}
 		slow = slow.Next
 		temp += 1
+		count++
+		fmt.Printf("%s | n: %d | ", "E", n)
+		printLog(temp, slow, fast)
+		// fmt.Println("S | n: 2 | temp: 0 | slow.Val: 1 | fast.Val: 1")
+		fmt.Println("-------------------------------------------------")
 	}
+	fmt.Println("finished loop")
 
+	fmt.Printf("%s | n: %d | ", "B", n)
+	printLog(temp, slow, fast)
 	if n == temp {
 		head = head.Next
 	} else {
 		fast.Next = fast.Next.Next
 	}
+	fmt.Println("balanced")
+	fmt.Printf("%s | n: %d | ", "F", n)
+	printLog(temp, slow, fast)
 
 	return head
+}
+
+func printLog(i int, slow, fast *linkedlist.ListNode) {
+	fmt.Printf("temp: %d | ", i)
+	if slow != nil {
+		fmt.Printf("slow.Val: %d | ", slow.Val)
+	} else {
+		fmt.Printf("slow.Val: nil | ")
+	}
+	if fast != nil {
+		fmt.Printf("fast.Val: %d\n", fast.Val)
+	} else {
+		fmt.Printf("fast.Val: nil\n")
+	}
 }
