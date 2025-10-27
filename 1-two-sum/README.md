309fa943b95f7a65090cc163d68ef8f72de53b46
Renato Spakauskas <renatospaka@gmail.com>
2025-10-27 16:52:32 -0300

    feat: :necktie: update solution for (1) Two Sum

--- 1-two-sum/README.md
+++ 1-two-sum/README.md
@@ -1,4 +1,41 @@
-## Two Sum
-(easy)
+## 1. Two Sum
 
-<a href="https://leetcode.com/problems/two-sum/">problem</a>
+### Problem
+
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+([LeetCode problem](https://leetcode.com/problems/two-sum/))
+
+### Approach
+
+Use a hash map to store the difference between the target and each element:
+
+- Iterate through the array.
+- For each element, check if it exists in the hash map.
+	- If it does, return the indices.
+	- If not, store the difference (target - current element) and its index in the hash map.
+
+Time complexity: $O(n)$  
+Space complexity: $O(n)$
+
+### Example
+
+Input: nums = [2,7,11,15], target = 9  
+Output: [0,1]  
+Explanation: nums[0] + nums[1] = 2 + 7 = 9
+
+Input: nums = [3,2,4], target = 6  
+Output: [1,2]
+
+### Usage
+
+See `main.go` for code to compute the indices of the two numbers that add up to the target.
+
+### Resources
+
+- [LeetCode problem](https://leetcode.com/problems/two-sum/)
+- [Two Sum explanation (YouTube)](https://youtu.be/KLlXCFG5TnA?si=iYqrignkMcZbOOwm)
--- 1-two-sum/main.go
+++ 1-two-sum/main.go
@@ -3,45 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	var (
-		list []int
-		target int
-		indexes []int
-	)
-	
-	target = 9
-	list = []int{2, 7, 11 ,15}
-	indexes = twoSum(list, target)
-	fmt.Printf("The indexes that satisfy sum=%d of array %v is %v\n", target, list, indexes)
-	
-	target = 6
-	list = []int{3, 2, 4}
-	indexes = twoSum(list, target)
-	fmt.Printf("The indexes that satisfy sum=%d of array %v is %v\n", target, list, indexes)
-	
-	target = 6
-	list = []int{3, 3}
-	indexes = twoSum(list, target)
-	fmt.Printf("The indexes that satisfy sum=%d of array %v is %v\n", target, list, indexes)
-	
-	target = 5
-	list = []int{5}
-	indexes = twoSum(list, target)
-	fmt.Printf("The indexes that satisfy sum=%d of array %v is %v\n", target, list, indexes)
+	test([]int{2, 7, 11, 15}, 9)
+	test([]int{3, 2, 4}, 6)
+	test([]int{3, 3}, 6)
+	test([]int{5}, 5)
 }
 
-func twoSum(list []int, target int) []int {
-	if len(list) < 2 {
+func test(nums []int, target int) {
+	indexes := twoSum(nums, target)
+	fmt.Printf("The indexes that satisfy sum=%d of array %v is %v\n", target, nums, indexes)
+}
+
+func twoSum(nums []int, target int) []int {
+	if len(nums) < 2 {
 		return []int{}
 	}
 	hash := make(map[int]int)
 
-	for ix, number := range list {
-		if existingNumber, ok := hash[target - number]; ok {
-			return []int{existingNumber, ix}
+	for i, n := range nums {
+		if existingNumber, ok := hash[target-n]; ok {
+			return []int{existingNumber, i}
 		}
 
-		hash[number] = ix
+		hash[n] = i
 	}
 
 	return []int{}
